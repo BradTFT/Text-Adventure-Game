ee898e9ced98c69d0b2bd8b661de69546be4b72e
BradTFT <bradtft@outlook.com>
2022-05-20 20:42:42 -0400

    Add working rolltest

---
+++ roll-test.py
@@ -0,0 +1,18 @@
+from doctest import OutputChecker
+import random
+
+attack = input('you need to attack this spider')
+attempt = random.randrange(1, 38)
+outcome = random.randrange(1, 38)
+failedattack = 'Your attack failed'
+passedattack = 'Your attack was sucessful'
+
+print(f'your roll: {attempt}')
+print(f"outcome: {outcome}")
+
+if outcome > attempt:
+    print(failedattack)
+elif outcome == attempt:
+    print(failedattack)
+elif outcome < attempt:
+    print(passedattack)
