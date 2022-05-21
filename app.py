74490abc2c42240604e740244c4a79bf3a130340
BradTFT <bradtft@outlook.com>
2022-05-20 21:15:30 -0400

    Add experimental functions

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 from roll import rollSystem #imports a background file running the rolling function
+
+inventory = ['a', 'x', 'b']
+#experimenting a little bit
+input = input('')
+
+
+def showInventory():
+    #const for printing inventory list
+    print('\n')
+    print(' \n \n'.join(inventory))
+    
+
+if input == 'e' or 'E':
+    showInventory()
+
+#basic idea rn
